5ac84080c121346149ca716902f47a70f2d232f8
krishpranav <krisna.pranav@gmail.com>
2021-05-13 19:01:23 +0530

    added function for get options

--- app/view/preferences.js
+++ app/view/preferences.js
@@ -25,5 +25,65 @@ $('.close').click(() => {
   ipc.on('getVersion', (event, version) => {
     window.version = version
 })
+
+ipc.on('getOptions', (event, options) => {
+    optionsObj = options
+  
+    for (const key in options.hosts) {
+      let service = options.hosts[key]
+  
+      let select = '#services select[name="uploadService"] > optgroup'
+      let option = `<option value="pomf:${service}">${service}</option>'`
+  
+      $(select).append(option)
+    }
+  
+    for (const key in options.history.reverse()) {
+      let screenshot = options.history[key]
+  
+      let element = `<li class="screenshot">
+      <div class="thumbnail"><span class="helper"></span><img src="${screenshot.url.link}" /></div>
+      <p><i class="icon ion-link"></i> <a href="${screenshot.url.link}" target="_blank">${screenshot.url.link}</a></p>
+      <p><i class="icon ion-clock"></i> <span style="opacity:0.75">${parseTime(screenshot.timestamp)}</span></p></li>`
+  
+      $('#history .history-container > ul').append(element)
+    }
+  
+    if (options.showIcon === true) { $('#showIcon').prop('checked', true) }
+  
+    if (options.deleteOnUpload === true) { $('#deleteOnUpload').prop('checked', true) }
+  
+    if (options.startAtLogin === true) { $('#startAtLogin').prop('checked', true) }
+  
+    if (options.shortcuts) {
+      for (const shortcut of Object.keys(options.shortcuts)) {
+        let combo = options.shortcuts[shortcut]
+  
+        $('#' + shortcut).val(combo)
+      }
+    }
+  
+    if (options.services && options.services.uploadService) {
+      $('select[name="uploadService"]').val(options.services.uploadService)
+    }
+  
+    if (options.services && options.services.shortenerService) {
+      $('select[name="shortenerService"]').val(options.services.shortenerService)
+    }
+  
+    if (options.customService) {
+      $('#servicePrompt input[name="uploadPath"]').val(options.customService.uploadPath)
+      $('#servicePrompt input[name="resultPath"]').val(options.customService.resultPath)
+      $('select[name="uploadService"]').val('custom')
+    }
+  })
+  
+  ipc.on('showService', (event, current) => {
+    showPrompt('#servicePrompt')
+  })
+  
+  ipc.on('showLoader', (event, current) => {
+    showPrompt('#loader')
+})
   
 
