12654a3fb4cd025d7ceaec352dbb21f28832166b
krishpranav <krisna.pranav@gmail.com>
2021-05-14 10:47:49 +0530

    update functions for adding screen shot to ftp server

---
+++ src/components/ftp.js
@@ -0,0 +1,25 @@
+const _ftp = require('jsftp')
+
+module.exports = class {
+  constructor (connection, directory, url) {
+    this.dir = directory
+    this.url = url
+    this.ftp = new _ftp({
+      host: connection.host,
+      port: connection.port,
+      user: connection.user,
+      pass: connection.pass
+    })
+}
+
+  upload (file, callback) {
+    console.log('Uploading image to FTP server...')
+
+    this.ftp.put(file, `${this.dir}/${file}`, (error) => {
+      if (!error) {
+        const result = { link: `${this.url}/${file}` }
+        callback(result)
+      }
+    })
+  }
+}
